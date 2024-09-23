7af02d34b6f43cbbfb4a5f2ae00933183c47be12
Duke-Raccoon <mr.artyom.2020@mail.ru>
2024-09-23 10:49:12 +0300

    My finished first projest

--- main.py
+++ main.py
@@ -2,8 +2,46 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    characters_dict = get_num_characters(text)
+    chars_sorted_list = chars_dict_to_sorted_list(characters_dict)
 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def get_num_characters(text):
+    characters = {}
+    for character in text:
+        lowered = character.lower()
+        if lowered in characters:
+            characters[lowered] += 1
+        else:
+            characters[lowered] = 1
+    return characters
 
 def get_num_words(text):
     words = text.split()
